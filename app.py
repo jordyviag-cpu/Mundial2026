f3711489f6f9d5777ef9c0c6585b6da2041d0279
jordyviag-cpu <jordy.vi.ag@gmail.com>
2026-06-07 02:34:37 -0500

    app.py

--- app.py
+++ app.py
@@ -20,3 +20,11 @@ st.write("Fecha:", fecha)
 
 if st.button("Grabar"):
     st.success("Pronóstico registrado")
+
+import streamlit as st
+from supabase import create_client
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
